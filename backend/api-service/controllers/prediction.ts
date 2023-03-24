014c2c39e5af0ca778d3f7c0313f2ab439481efc
j2002chen <j.2002chen@gmail.com>
2023-03-24 12:06:19 -0400

    added some unsaved changes

--- backend/api-service/controllers/prediction.ts
+++ backend/api-service/controllers/prediction.ts
@@ -1,9 +1,11 @@
-import { makePrediction } from '../prediction-service/models/prediction';
+import { Request, Response } from 'express';
+import { makePrediction } from '../../prediction-service/prediction';
+import { getYahooWeeklyMatchupData } from './apis/yahoo';
 
 async function getOptimizedLineup(req: Request, res: Response) {
   const weeklyMatchupData = await getYahooWeeklyMatchupData();
-  
   const optimizedLineup = await makePrediction(weeklyMatchupData);
-  
   res.json({ optimizedLineup });
 }
+
+export { getOptimizedLineup };
--- backend/api-service/index.ts
+++ backend/api-service/index.ts
@@ -1,24 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import { ApolloServer } from 'apollo-server-express';
-import { typeDefs, resolvers } from '../../frontend/server/graphql'; // Import your GraphQL schema and resolvers
+import { typeDefs, resolvers } from '../../frontend/server/graphql'; 
 import { getOptimizedLineup } from './controllers/prediction';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// API routes
 app.get('/predictions/optimized-lineup', getOptimizedLineup);
 
-// Set up the GraphQL server
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 apolloServer.applyMiddleware({ app });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   console.log(`GraphQL server is running on http://localhost:${PORT}${apolloServer.graphqlPath}`);
